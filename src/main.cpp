53cec9da07619190abfa31a692a17dbe289f4e94
Michael-Loula <mloula@icloud.com>
2020-08-11 14:50:11 -0500

    setup build stuff

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <SDL.h>
+
+
+void setup_gfx() {
+  return;
+}
+
+void setup_input() {
+  return;
+}
+
+void draw() {
+  return;
+}
+
+int main() {
+  /*
+    do graphics
+    setup render system and register input callbacks
+  */
+  //initialize 
+  //myChip.init();
+  //load game
+  //myChip.loadGame("pong");
+  //emu loop
+  //while(true) {
+  //myChip.emulateCycle();
+  //if (myChip.df) {
+      //draw
+  // }
+    // store key press state
+    //myChip.setKeys();
+//}
+return 0;
+}
