b873f3011a42abf9b4e06ce27a8d38df23b89474
Michael Loula <michaelloula@Michaels-MacBook-Pro.local>
2020-12-22 20:20:57 -0600

    cleanup

--- ceemu_main.c
+++ ceemu_main.c
@@ -8,11 +8,12 @@ SDL_Window *window;
 SDL_Event event;
 
 static void _sig_handler(int signo){
-  if (signo == SIGTERM || signo == SIGINT || signo == SIGSEGV){
+  if (signo == SIGTERM || signo == SIGINT || signo == SIGSEGV || signo == SIGABRT){
       SDL_DestroyRenderer(renderer);
       SDL_DestroyWindow(window);
       SDL_Quit();
   }
+  exit(0);
 }
 
 uint8_t keymap[16] = {
@@ -38,96 +39,132 @@ int main(int argc, char **argv) {
     if (signal(SIGINT, _sig_handler) == SIG_ERR) {
         fprintf(stderr,"Can't catch SIGINT...exiting.\n");
     }
-
     if (signal(SIGTERM, _sig_handler) == SIG_ERR) {
         fprintf(stderr,"Can't catch SIGTERM...exiting.\n");
     }
     if (signal(SIGSEGV, _sig_handler) == SIG_ERR) {
         fprintf(stderr,"Can't catch SIGSEGV...exiting.\n");
     }
-
+    if (signal(SIGABRT, _sig_handler) == SIG_ERR) {
+        fprintf(stderr,"Can't catch SIGABRT...exiting.\n");
+    }
     //declares processor state onto stack
     cpu chip8;
     //initializes processor state
     cpu_init(&chip8);
-
     
+    //sdl setup
     SDL_Init(SDL_INIT_VIDEO);
-
     SDL_CreateWindowAndRenderer(1024, 512, 0, &window, &renderer);
     SDL_RenderSetLogicalSize(renderer, 1024, 512);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
     SDL_RenderClear(renderer);
+
     //loads rom directly into memory using read
-    //cpu_ldrm(&chip8,"chip8-test-rom/test_opcode.ch8");
-    cpu_ldrm(&chip8,"pong.rom");
+
+    cpu_ldrm(&chip8,"a.ch8");
+
     //go
     for(;;) {
+
         //fetches and executes opcode
-        cycle_next(&chip8);   
-        //printf("30E: %x\n",chip8.mem[0x130E & 0x0FFF]);
+        cycle_next(&chip8);  
+
         //ticks down timers
         timers_next(&chip8);
 
-        //draw if need be
+        //draw if need be 
         if (chip8.dflag) {
             
             //draw
             SDL_Rect rct;
+
+            //constant multiple of 64 x 32
             rct.w = 16;
             rct.h = 16;
+
+            //render blocks
             for (unsigned int i = 0; i < (64*32); i++) {
+
                     unsigned int x = i / 32;
                     unsigned int y = i % 32;
+
                     rct.x = x*16;
                     rct.y = y*16;
+
                     if (chip8.gfx[(64*y)+x]) {
+
+                        //green
                         SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
 
                     }
                     else {
+
+                        //black
                         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
                     }
+
                     SDL_RenderDrawRect(renderer,&rct);
                     SDL_RenderFillRect(renderer,&rct);
                 
             }
 
+            //show
             SDL_RenderPresent(renderer);
             chip8.dflag = 0;
+
         }
+
+        //sleep
         usleep(1000);
+
         if (SDL_PollEvent(&event)) {
+
             switch (event.type) {
+
                 case SDL_QUIT:
+
                     SDL_DestroyRenderer(renderer);
                     SDL_DestroyWindow(window);
                     SDL_Quit();
                     return 0;
-                    //break;
+
                 //check keypress
                 case SDL_KEYDOWN:
+
                     //set key
                     for (int i = 0; i < 16; ++i) {
+
                         if (event.key.keysym.sym == keymap[i]) {
                             chip8.key[i] = 1;
                         }
+
                     }
                     break;
+
                 case SDL_KEYUP:
+
                     //set key
                     for (int i = 0; i < 16; ++i) {
+
                         if (event.key.keysym.sym == keymap[i]) {
                             chip8.key[i] = 0;
                         }
+
                     }
                     break;
+
                 default: 
+
                     break;
+
             }
+            
         }
             
     }
+
+    //quit
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
--- cpulib.c
+++ cpulib.c
@@ -5,83 +5,118 @@
 #include <unistd.h>
 #include <SDL2/SDL.h>
 
-unsigned char chip8_fontset[80] =
-{ 
-  0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-  0x20, 0x60, 0x20, 0x20, 0x70, // 1
-  0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-  0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-  0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-  0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-  0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-  0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-  0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-  0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-  0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-  0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-  0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-  0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-  0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-  0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+unsigned char chip8_fontset[80] = { 
+
+  0xF0, 0x90, 0x90, 0x90, 0xF0,
+  0x20, 0x60, 0x20, 0x20, 0x70,
+  0xF0, 0x10, 0xF0, 0x80, 0xF0,
+  0xF0, 0x10, 0xF0, 0x10, 0xF0,
+  0x90, 0x90, 0xF0, 0x10, 0x10,
+  0xF0, 0x80, 0xF0, 0x10, 0xF0,
+  0xF0, 0x80, 0xF0, 0x90, 0xF0,
+  0xF0, 0x10, 0x20, 0x40, 0x40,
+  0xF0, 0x90, 0xF0, 0x90, 0xF0,
+  0xF0, 0x90, 0xF0, 0x10, 0xF0,
+  0xF0, 0x90, 0xF0, 0x90, 0x90,
+  0xE0, 0x90, 0xE0, 0x90, 0xE0,
+  0xF0, 0x80, 0x80, 0x80, 0xF0,
+  0xE0, 0x90, 0x90, 0x90, 0xE0,
+  0xF0, 0x80, 0xF0, 0x80, 0xF0,
+  0xF0, 0x80, 0xF0, 0x80, 0x80
+
 };
 
 int cpu_fetch(cpu* chip8) {
+
     chip8->opcode = chip8->mem[chip8->pc] << 8 | chip8->mem[chip8->pc + 1];
     return 0;
+
 }
+
 //advance timers
 int timers_next(cpu* chip8)  {
+
     if (chip8->dt > 0) {
+
         chip8->dt--;
+
     }
     if (chip8->st > 0) {
+
         if (chip8->st == 1) {
+
+            //beep!
             puts("beep");
+
         }
+
         chip8->st--;
     }
+
     return 0;
+
 }
+
+//initialize cpu struct onto stack
 int cpu_init(cpu* chip8) {
+
     chip8->pc = 0x200;
     chip8->opcode = 0;
     chip8->I = 0;
     chip8->sp = 0;
-    chip8->dflag = 0; 
+    chip8->dflag = 0;
+
     memset(chip8->mem,0,4096);
     memset(chip8->gfx,0,64*32);
     memset(chip8->stack,0,16);
     memset(chip8->V,0,16);
     memset(chip8->key,0,16);
     memcpy(chip8->mem,chip8_fontset,80);
+
     return 0;
 }
 
+//load rom into main memory **REQUIRES POSIX COMPLIANT OS**
 int cpu_ldrm(cpu* chip8, const char* rom_name) {
+
     int fd = open(rom_name,O_RDONLY);
+
     size_t size = lseek(fd, 0, SEEK_END);
     lseek(fd, 0, SEEK_SET);
+
     read(fd,chip8->mem+512,size);
+
     close(fd);
+
     return 0;
 }
 
+
+//advance CPU -- fetches and executes opcode at pc
 int cycle_next(cpu* chip8) {
+
     cpu_fetch(chip8);
     cpu_exec(chip8);
+
     return 0;
+
 }
 
 int cpu_exec(cpu* chip8) {
+
     /*big ol switch statement*/
-    //printf("%X\n",chip8->opcode);
     switch(chip8->opcode & 0xF000) {
+
         case 0xA000:
+
             chip8->I = chip8->opcode & 0x0FFF;
             chip8->pc += 2;
             break;
+
         case 0x0000:
+
             switch(chip8->opcode & 0x000F) {
+
                 case 0x0000:
                     memset(chip8->gfx,0,64*32);
                     chip8->dflag = 1;
@@ -89,172 +124,249 @@ int cpu_exec(cpu* chip8) {
                     break;
         
                 case 0x000E:
+
                     chip8->sp -= 1;
                     chip8->pc = chip8->stack[chip8->sp];
                     chip8->pc += 2;
                     break;
+
                 default:
+
                     break;
+
             }
+
             break;
+
         case 0x1000:
-            //abort();
+
             chip8->pc = (0x0FFF & chip8->opcode);
             break;
+
         case 0x2000:
+
             chip8->stack[chip8->sp] = chip8->pc;
             chip8->sp += 1;
             chip8->pc = 0x0FFF & chip8->opcode;
             break;
+
         case 0x3000:
+
             if (chip8->V[(chip8->opcode & 0x0F00) >> 8] == (chip8->opcode & 0x0FF)) {
                 chip8->pc += 2;
             }
             chip8->pc += 2;
             break;
+
         case 0x4000:
+
             if (chip8->V[(chip8->opcode & 0x0F00) >> 8] != (chip8->opcode & 0x0FF)) {
                 chip8->pc += 2;
             }
             chip8->pc += 2;
             break;
+
         case 0x5000:
+
             if (chip8->V[(chip8->opcode & 0x0F00) >> 8] == chip8->V[(chip8->opcode & 0x00F0) >> 4]) {
                 chip8->pc += 2;
             }
             chip8->pc += 2;
             break;
+
         case 0x6000:
+
             chip8->V[(chip8->opcode & 0x0F00) >> 8] = (chip8->opcode & 0x00FF);
             chip8->pc += 2;
             break;
+
         case 0x7000:
+
             chip8->V[(chip8->opcode & 0x0F00) >> 8] += (chip8->opcode & 0x00FF);
             chip8->pc += 2;
             break;
+
         case 0x8000:
+
             switch(chip8->opcode & 0x000F) {
+
                 case 0x0000:
+
                     chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->V[(chip8->opcode & 0x00F0) >> 4];
                     chip8->pc += 2;
                     break;
+
                 case 0x0001:
+
                     chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->V[(chip8->opcode & 0x0F00) >> 8] | chip8->V[(chip8->opcode & 0x00F0) >> 4];
                     chip8->pc += 2;
                     break;
+
                 case 0x0002:
+
                     chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->V[(chip8->opcode & 0x0F00) >> 8] & chip8->V[(chip8->opcode & 0x00F0) >> 4];
                     chip8->pc += 2;
                     break;
+
                 case 0x0003:
+
                     chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->V[(chip8->opcode & 0x0F00) >> 8] ^ chip8->V[(chip8->opcode & 0x00F0) >> 4];
                     chip8->pc += 2;
                     break;
+
                 case 0x0004:
-                    //chip8->V[15] = (chip8->V[(chip8->opcode & 0x0F00) >> 8] > (0x00FF - chip8->V[(chip8->opcode & 0x00F0) >> 4]));
-                    //chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->V[(chip8->opcode & 0x0F00) >> 8] + chip8->V[(chip8->opcode & 0x00F0) >> 4];
+
                     chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->V[(chip8->opcode & 0x0F00) >> 8] + chip8->V[(chip8->opcode & 0x00F0) >> 4];
                     chip8->V[15] = ((0xFF - chip8->V[(chip8->opcode & 0x0F00) >> 8]) < (chip8->V[(chip8->opcode & 0x00F0) >> 4]));
-                    //printf("%X\n",chip8->V[15]);
                     chip8->pc += 2;
                     break;
+
                 case 0x0005:
-                    
+
                     chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->V[(chip8->opcode & 0x0F00) >> 8] - chip8->V[(chip8->opcode & 0x00F0) >> 4];
                     chip8->V[15] = (chip8->V[(chip8->opcode & 0x0F00) >> 8] < chip8->V[(chip8->opcode & 0x00F0) >> 4]);
                     chip8->pc += 2;
-                    //printf("%X\n",chip8->V[15]);
                     break;
+
                 case 0x0006:
-                    //chip8->V[15] = chip8->V[(chip8->opcode & 0x0F00) >> 8] & 0x000F;
+
                     chip8->V[15] = chip8->V[(chip8->opcode & 0x0F00) >> 8] & 0x1;
                     chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->V[(chip8->opcode & 0x0F00) >> 8] >> 1;
                     chip8->pc += 2;
                     break;
+
                 case 0x0007:
+
                     chip8->V[15] = (chip8->V[(chip8->opcode & 0x0F00) >> 8] < chip8->V[(chip8->opcode & 0x00F0) >> 4]);
                     chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->V[(chip8->opcode & 0x00F0) >> 4] - chip8->V[(chip8->opcode & 0x0F00) >> 8];
                     chip8->pc += 2;
                     break;
+
                 case 0x000E:
+
                     chip8->V[15] = chip8->V[(chip8->opcode & 0x0F00) >> 8] >> 7;
                     chip8->V[(chip8->opcode & 0x0F00) >> 8] = (chip8->V[(chip8->opcode & 0x0F00) >> 8] << 1);
                     chip8->pc += 2;
                     break;
+
             }
+
             break;
+
         case 0x9000:
-            if (chip8->V[(chip8->opcode & 0x0F00) >> 8] == chip8->V[(chip8->opcode & 0x00F0) >> 4]) {
+
+            if (chip8->V[(chip8->opcode & 0x0F00) >> 8] == chip8->V[(chip8->opcode & 0x00F0) >> 4]) { 
                 chip8->pc += 2;
             }
+
             chip8->pc += 2;
             break;
+
         case 0xB000:
+
             chip8->pc = chip8->V[0] + (chip8->opcode & 0x0FFF);
             break;
+
         case 0xC000:
+
             chip8->V[(chip8->opcode & 0x0F00) >> 8] = ((rand() % 256) & (chip8->opcode & 0x00FF));
             chip8->pc += 2;
             break;
-        case 0xD000: 
+
+        case 0xD000:
+
             {
-            //abort();
+            
             uint16_t x = chip8->V[(chip8->opcode & 0x0F00) >> 8];
             uint16_t y = chip8->V[(chip8->opcode & 0x00F0) >> 4];
+
             if (x >= 64) x %= 64;
             if (y >= 32) y %= 32;
-            uint16_t h = h = chip8->opcode & 0x000F;
-            uint16_t pixel;
-            chip8->V[0xF] = 0;
 
-            for (int yl = 0; yl < h; yl++)  {
+            uint16_t h = h = chip8->opcode & 0x000F;
+
+            uint16_t pixel;
+
+            chip8->V[15] = 0;
+
+            for (int yl = 0; yl < h; yl++) {
+
                 pixel = chip8->mem[chip8->I + yl];
+
                 for(int xl = 0; xl < 8; xl++) {
     
                     if((pixel & (0x80 >> xl)) != 0) {
+
                         if(chip8->gfx[(x + xl + ((y + yl) * 64))] == 1) {
-                            chip8->V[0xF] = 1;               
-                        }                  
-                        //puts("coll");
+                            chip8->V[15] = 1;
+
+                        }
+
                         chip8->gfx[x + xl + ((y + yl) * 64)] ^= 1;
                     }
+
                 }
+
             }
+
             chip8->dflag = 1;
             chip8->pc += 2;
+
             break;
+
             }
         case 0xE000:
+
             switch(chip8->opcode & 0x00FF) {
+
                 case 0x009E:
+
                     if (chip8->key[chip8->V[(chip8->opcode & 0x0F00) >> 8]] != 0) {
                         chip8->pc += 2;
                     }
+
                     chip8->pc += 2;
                     break;
+
                 case 0x00A1:
+
                     if (chip8->key[chip8->V[(chip8->opcode & 0x0F00) >> 8]] == 0) {
                         chip8->pc += 2;
                     }
+
                     chip8->pc += 2;
                     break;
+
             }
+
             break;
+
         case 0xF000:
+
             switch(chip8->opcode & 0x00FF) {
+
                 case 0x0007:
+
                     chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->dt;
                     chip8->pc += 2;
                     break;
+
                 case 0x000A:
+
                     {
+
                     int flag = 0;
+
                     for (int i = 0; i < 16; i++) {
+
                         if (chip8->key[i] != 0) {
+
                             chip8->V[(chip8->opcode & 0x0F00) >> 8] = i;
                             flag = 1;
                         }
+
                     }
+
                     if (!flag) {
                         //do not increment pc
                         break;
@@ -262,49 +374,73 @@ int cpu_exec(cpu* chip8) {
 
                     chip8->pc += 2;
                     break;
+
                     }
+
                 case 0x0015:
+
                     chip8->dt = chip8->V[(chip8->opcode & 0x0F00) >> 8];
                     chip8->pc += 2;
                     break;
+
                 case 0x0018:
+
                     chip8->st = chip8->V[(chip8->opcode & 0x0F00) >> 8];
                     chip8->pc += 2;
                     break;
+
                 case 0x001E:
+
                     chip8->I += chip8->V[(chip8->opcode & 0x0F00) >> 8];
                     chip8->pc += 2;
                     break;
+
                 case 0x0029:
+
                     chip8->I = 0x5 * chip8->V[(chip8->opcode & 0x0F00) >> 8];
                     chip8->pc += 2;
                     break;
+
                 case 0x0033:
+
                     chip8->mem[chip8->I] = chip8->V[(chip8->opcode & 0x0F00) >> 8] / 100;
                     chip8->mem[chip8->I+1] = (chip8->V[(chip8->opcode & 0x0F00) >> 8] / 10) % 10;
                     chip8->mem[chip8->I+2] = ((chip8->V[(chip8->opcode & 0x0F00) >> 8] % 100) % 10);
                     chip8->pc += 2;
                     break;
+
                 case 0x0055:
-                    for (int i = 0; i < ((chip8->opcode & 0x0F00) >> 8)+1; i++) {
-                        chip8->mem[chip8->I+i] = chip8->V[i];
-                    }
+
+                    {
+                    void* ptr = &chip8->mem;
+                    memcpy(ptr+chip8->I,&chip8->V,((chip8->opcode & 0x0F00) >> 8)+1);
                     chip8->pc += 2;
                     break;
+                    }
+
                 case 0x0065:
-                    for (int i = 0; i < ((chip8->opcode & 0x0F00) >> 8)+1; i++) {
-                        chip8->V[i] = chip8->mem[chip8->I+i];
-                    }
+
+                    {
+                    void* ptr = &chip8->mem;
+                    memcpy(&chip8->V,ptr+chip8->I,((chip8->opcode & 0x0F00) >> 8)+1);
                     chip8->pc += 2;
                     break;
+                    }
+
                 default:
+
                     exit(0);
                     break;
+
             }
+
             break;
+
         default:
+
             printf("unknown instruction: 0x%X\n", chip8->opcode);
             
     }
+    
     return 0;
 }
